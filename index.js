b6f9b0bc7915ddea831671e6df4c93ac967314c7
Sam Sharma <samsharma.dev@gmail.com>
2024-06-30 11:25:24 -0700

    set up express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`);
+})
