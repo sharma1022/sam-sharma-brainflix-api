9496d24f7723757a72d6a841699e8650e583aaa9
Sam Sharma <samsharma.dev@gmail.com>
2024-06-30 12:26:03 -0700

    set up express router

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const videoRoutes = require("./routes/videos");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
+
+app.use("/videos", videoRoutes);
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`);
