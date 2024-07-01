dec895de6d163c664d8b28a9f2a91386fd363409
Sam Sharma <samsharma.dev@gmail.com>
2024-06-30 21:49:12 -0700

    Post comment route working

--- routes/videos.js
+++ routes/videos.js
@@ -2,16 +2,14 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const randomName = require('random-name');
 
 const videosFile = path.join(__dirname, "../data/video-details.json");
 
-const readFile = (file, callback) => {
-  fs.readFile(file, "utf8", callback);
-}
-
 //get all videos
 router.get("/", (req, res) => {
-  readFile(videosFile, (err, data) => {
+  fs.readFile(videosFile, "utf8", (err, data) => {
     if (err) {
       return res.status(500).send(err);
     }
@@ -27,18 +25,51 @@ router.get("/", (req, res) => {
 });
 
 //get selected video
-router.get("/:id", (req,res) => {
-    readFile(videosFile, (err, data) => {
+router.get("/:id", (req, res) => {
+  fs.readFile(videosFile, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    const videoId = req.params.id;
+
+    if (JSON.parse(data).find((video) => video.id === videoId) === undefined) {
+      return res.status(400).send("Video not found");
+    }
+    res
+      .status(200)
+      .json(JSON.parse(data).find((video) => video.id === videoId));
+  });
+});
+
+//post comment
+router.post("/:id/comments", (req, res) => {
+  if (req.body.comment === "") {
+    return res.status(400).send("No comment body.");
+  }
+  fs.readFile(videosFile, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    const videoId = req.params.id;
+    const videoData = JSON.parse(data);
+    const selectedVideo = videoData.find((video) => video.id === videoId);
+
+    const newComment = {
+        id: uuidv4(),
+        name: randomName.first() + " " + randomName.last(),
+        comment: req.body.comment,
+        likes:0,
+        timestamp: Date.now()
+    }
+    selectedVideo.comments.push(newComment);
+
+    fs.writeFile(videosFile, JSON.stringify(videoData), (err) => {
         if(err){
             return res.status(500).send(err);
         }
-        const videoId = req.params.id;
-
-        if (JSON.parse(data).find((video) => video.id === videoId) === undefined){
-            return res.status(400).send("Video not found");
-        }
-        res.status(200).json(JSON.parse(data).find((video) => video.id === videoId));
+        res.status(201).send(newComment);
     })
-})
+  });
+});
 
 module.exports = router;
