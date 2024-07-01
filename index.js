a80b7befc584e18d4e36f0cc4937b406255c3328
Sam Sharma <samsharma.dev@gmail.com>
2024-06-30 20:06:11 -0700

    get videos route working

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const cors = require("cors");
 const videoRoutes = require("./routes/videos");
 
 require("dotenv").config();
-
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use(express.static("public"));
 
 app.use("/videos", videoRoutes);
 
--- routes/videos.js
+++ routes/videos.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const path = require("path");
 
-function readFile(file, callback) {
+const videosFile = path.join(__dirname, "../data/video-details.json");
+
+const readFile = (file, callback) => {
   fs.readFile(file, "utf8", callback);
 }
 
 router.get("/", (req, res) => {
-  readFile("./data/video-details.json", (err, data) => {
+  readFile(videosFile, (err, data) => {
     if (err) {
       return res.status(500).send(err);
     }
@@ -22,8 +25,5 @@ router.get("/", (req, res) => {
   });
 });
 
-// router.get("/", (req, res) => {
-//     res.send("Hello World!!!!");
-// })
 
 module.exports = router;
