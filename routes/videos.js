c3a3b118439cefde0b2aca3bc15e0c26ff72e0f3
Sam Sharma <samsharma.dev@gmail.com>
2024-06-30 17:14:22 -0700

    added images to the images folder

--- routes/videos.js
+++ routes/videos.js
@@ -3,25 +3,24 @@ const router = express.Router();
 const fs = require("fs");
 
 function readFile(file, callback) {
-    fs.readFile(file, "utf8", callback);
+  fs.readFile(file, "utf8", callback);
 }
 
-router.get("/",
-  (req, res) => {
-    readFile("./data/video-details.json", (err, data) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(200).json(
-            JSON.parse(data).map((video) => ({
-                id: video.id,
-                title: video.title,
-                channel: video.channel,
-                image: video.image,
-            }))
-      );
-    });
+router.get("/", (req, res) => {
+  readFile("./data/video-details.json", (err, data) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.status(200).json(
+      JSON.parse(data).map((video) => ({
+        id: video.id,
+        title: video.title,
+        channel: video.channel,
+        image: video.image,
+      }))
+    );
   });
+});
 
 // router.get("/", (req, res) => {
 //     res.send("Hello World!!!!");
