d30b6e86f4149195fcb08c546a01df9878729e75
Sam Sharma <samsharma.dev@gmail.com>
2024-06-30 17:09:33 -0700

    able to get the id, title, channel and image data from the json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const videoRoutes = require("./routes/videos");
 
 require("dotenv").config();
@@ -8,6 +9,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
+
+app.use(express.json());
 
 app.use("/videos", videoRoutes);
 
