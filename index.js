1d0b9fb3c57e4a59d1ba53f325cda63da838ce31
Sam Sharma <samsharma.dev@gmail.com>
2024-10-14 00:33:35 -0700

    added vercel.json

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const videoRoutes = require("./routes/videos");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors({ origin :"http://localhost:3000" }));
+app.use(cors({ origin :"https://brainflix-chi.vercel.app/" }));
 
 app.use(express.json());
 
 app.use(express.static("public"));
 
 app.use("/videos", videoRoutes);
+app.get("/",(req,res)=> {
+    res.send("Welcome to Brainflix API");
+})
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`);
